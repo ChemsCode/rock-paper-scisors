a6cb4c4f0eb7f656d8d702f5ca0e9d1c17015483
ChemsCode <saidiceb@gmail.com>
2022-01-07 12:51:59 -0500

    we can play the game with buttons now

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function checkForWinRock(selection){
     switch(selection){           
         case "paper":
             console.log("COMPUTER WINS!");
+            displayWinnerText.textContent = "COMPUTER WINS!";
             return "COMPUTER WINS!";
         break;
 
         case "scisor":
             console.log("PLAYER WINS!");
+            displayWinnerText.textContent = "PLAYER WINS!";
             return "PLAYER WINS!";
         break;
         }
@@ -17,11 +19,13 @@ function checkForWinPaper(selection){
     switch(selection){           
         case "rock":
             console.log("PLAYER WINS!");
+            displayWinnerText.textContent = "PLAYER WINS!";
             return "PLAYER WINS!";
         break;
 
         case "scisor":
             console.log("COMPUTER WINS!");
+            displayWinnerText.textContent = "COMPUTER WINS!";
             return "COMPUTER WINS!";
         break;
         }
@@ -31,11 +35,13 @@ function checkForWinScisor(selection){
     switch(selection){           
         case "rock":
             console.log("COMPUTER WINS!");
+            displayWinnerText.textContent = "COMPUTER WINS!";
             return "COMPUTER WINS!";
         break;
 
         case "paper":
             console.log("PLAYER WINS!");
+            displayWinnerText.textContent = "PLAYER WINS!";
             return "PLAYER WINS!";
         break;
         }
@@ -62,10 +68,27 @@ function computerPlay(){
         break;
     }
 }
+
+function playerChose(){
+
+    rockBtn.addEventListener( 'click', () => {
+        return "rock";
+    });
+    paperBtn.addEventListener( 'click', () => {
+        return "paper";
+    });
+    paperBtn.addEventListener( 'click', () => {
+        return "scisor";
+    });
+
+}
+
 function playRound(playerSelection, computerSelection){
+
 
     if(playerSelection === computerSelection){
         console.log("tie!");
+        displayWinnerText.textContent = "tie!";
         return "tie!";
     }
     else if(playerSelection === "rock"){
@@ -84,30 +107,60 @@ let computerScore = 0;
 let playerScore = 0;
 let result;
 
-while(computerScore != 5 && playerScore !=5){
-    let player = prompt("Play rock, paper or scisors!");
-    let computer = computerPlay();
-    console.log("Computer choses: " + computer);
-    console.log("Player choses: " + player);
-    result = playRound(player,computer);
-    if(result === "tie!")
-    {
-        continue;
-    }
-    else if(result === "COMPUTER WINS!")
-    {
-        computerScore++;
-    }
-    else if(result === "PLAYER WINS!")
-    {
-        playerScore++;
-    }
-}
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scisorBtn = document.querySelector("#scisor");
 
-if(playerScore === 5){
-    console.log("PLAYER WINS THE WHOLE GAME!")
-}
-else{
-    console.log("COMPUTER WINS THE WHOLE GAME!")
-}
 
+// while(computerScore != 5 && playerScore !=5){
+//     let player = playerChose();
+//     let computer = computerPlay();
+//     console.log("Computer choses: " + computer);
+//     console.log("Player choses: " + player);
+//     result = playRound(player,computer);
+//     if(result === "tie!")
+//     {
+//         continue;
+//     }
+//     else if(result === "COMPUTER WINS!")
+//     {
+//         computerScore++;
+//     }
+//     else if(result === "PLAYER WINS!")
+//     {
+//         playerScore++;
+//     }
+// }
+
+// if(playerScore === 5){
+//     console.log("PLAYER WINS THE WHOLE GAME!")
+// }
+// else{
+//     console.log("COMPUTER WINS THE WHOLE GAME!")
+// }
+
+    const displayWinnerContainer = document.querySelector('#displayWinner');
+
+    const displayWinnerText = document.createElement('div');
+    displayWinnerText.textContent = 'Winner: ';
+
+    displayWinnerContainer.appendChild(displayWinnerText);
+
+    let player;
+    let computer;
+
+    rockBtn.addEventListener( 'click', () => {
+        player = "rock";
+        computer = computerPlay();
+        result = playRound(player,computer);
+    });
+    paperBtn.addEventListener( 'click', () => {
+        player = "paper";
+        computer = computerPlay();
+        result = playRound(player,computer);
+    });
+    paperBtn.addEventListener( 'click', () => {
+        player = "scisor";
+        computer = computerPlay();
+        result = playRound(player,computer);
+    });
